659dc92fcb1fe1ab3ee04b942cf23cfe8d81d4fd
unknown <carlos.chavez1@ujcv.edu.hn>
2024-02-04 09:49:10 -0600

    actualizando archivos

---
+++ components/evaluacion/Evaluacion.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, Image, Button, StyleSheet } from 'react-native';
+import StarEmpty from './StarEmpty';
+import StarFilled from './StarFilled';
+
+
+export type EvalProps = {
+    calificacion: number;
+    onCalificacionChange: any;
+};
+
+export type StarProp = {
+    posicion:number;
+    onClick:any;
+
+};
+
+const Evaluacion = (props: EvalProps) => {
+      const handleOnClick = (posicion:number) => {
+        props.onCalificacionChange(posicion+1)
+      }
+    return (
+    <View style ={styles.contenedor}>
+        {props.calificacion == 0 && (<StarEmpty onClick={handleOnClick} posicion={0}/>)}
+        {props.calificacion >= 1 && (<StarFilled onClick={handleOnClick} posicion={0}/>)}
+        {props.calificacion < 2 && (<StarEmpty onClick={handleOnClick} posicion={1}/>)}
+        {props.calificacion >= 2 && (<StarFilled onClick={handleOnClick} posicion={1}/>)}
+        {props.calificacion < 3 && (<StarEmpty onClick={handleOnClick} posicion={2}/>)}
+        {props.calificacion >= 3 && (<StarFilled onClick={handleOnClick} posicion={2}/>)}
+        {props.calificacion < 4 && (<StarEmpty onClick={handleOnClick} posicion={3}/>)}
+        {props.calificacion >= 4 && (<StarFilled onClick={handleOnClick} posicion={3}/>)}
+        {props.calificacion < 5 && (<StarEmpty onClick={handleOnClick} posicion={4}/>)}
+        {props.calificacion >= 5 && (<StarFilled onClick={handleOnClick} posicion={5}/>)}
+         
+    </View>
+    );
+};
+const styles = StyleSheet.create({
+    contenedor:{
+        display: 'flex', flexDirection:'row',
+    }
+})
+
+export default Evaluacion
